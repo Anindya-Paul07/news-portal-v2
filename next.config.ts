2f50dbda2f1c0ee4e362ef6346f4a9a666998d8d
Anindya-Paul07 <srutibhowmick4@gmail.com>
2025-12-17 16:16:46 +0600

    temporarily allowed image from all sources

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,9 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "backoffice.thecontemporary.news" },
+      // Temporarily allow images from any remote source (http/https).
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
     ],
   },
 };
